9bec90d32946ed9f1611bcd0f51c73966a3168a8
Hanson <hnhn789@yahoo.com.tw>
2017-03-11 16:37:53 +0800

    gogo

--- accounts/views.py
+++ accounts/views.py
@@ -21,16 +21,16 @@ class ChangePasseordView(APIView):
         if request.session.has_key('username'):
             user = User.objects.get(username=request.session['username'])
         else:
-            return Response({"message": '請先重新登入','success': False}, status=200)
+            return Response({"messages": '請先重新登入','success': False}, status=200)
 
         if user is not None:
 
             if not user.check_password(oldpassword):
-                return Response({"message": '原本密碼錯誤', 'success': False}, status=200)
+                return Response({"messages": '原本密碼錯誤', 'success': False}, status=200)
             user.set_password(newpassword)
             user.save()
 
-            return Response({"message": '重設成功', 'success': True}, status=200)
+            return Response({"messages": '重設成功', 'success': True}, status=200)
 
 
 class ResendView(APIView):
@@ -39,10 +39,10 @@ class ResendView(APIView):
         try:
             user = User.objects.get(username=username)
         except ObjectDoesNotExist:
-            return Response({"message": '請先註冊此信箱', 'success': False}, status=200)
+            return Response({"messages": '請先註冊此信箱', 'success': False}, status=200)
 
         EmailConfirmation.objects.verify_email_for_object(email, user)
-        return Response({"message": '認證信已寄出！請確認！', 'success': True}, status=200)
+        return Response({"messages": '認證信已寄出！請確認！', 'success': True}, status=200)
 
 
 class SignUpView(APIView):
@@ -60,19 +60,19 @@ class SignUpView(APIView):
         email = username +'@ntu.edu.tw'
 
         if username == '' or password == '' or realname == '' or department=='':
-            return Response({"message": '註冊資料不完全', 'success': False}, status=200)
+            return Response({"messages": '註冊資料不完全', 'success': False}, status=200)
 
         if User.objects.filter(username=username).exists():
-            return Response({"message": '此信箱已被註冊過', 'success': False}, status=200)
+            return Response({"messages": '此信箱已被註冊過', 'success': False}, status=200)
 
         user = User.objects.create_user(username=username,password=password, first_name = department,last_name = realname)
 
         EmailConfirmation.objects.verify_email_for_object(email, user)
 
         if user is not None and user.is_active:
-            return Response({"message":'認證信已寄出！請確認！','success':True}, status=200)
+            return Response({"messages":'認證信已寄出！請確認！','success':True}, status=200)
         else:
-            return Response({"message": '註冊失敗，請重試', 'success': False}, status=200)
+            return Response({"messages": '註冊失敗，請重試', 'success': False}, status=200)
 
 
 class LoginView(APIView):
@@ -83,18 +83,18 @@ class LoginView(APIView):
 
             if user is not None:
                 if user.email == '':
-                    return Response({"message": '信箱尚未認證', 'success': False}, status=200)
+                    return Response({"messages": '信箱尚未認證', 'success': False}, status=200)
                 auth.login(request, user)
                 try:
                     ProfileUser = UserProfile.objects.get(user=user)
                 except ObjectDoesNotExist:
                     ProfileUser = UserProfile.objects.create(user=user)
                 points = ProfileUser.usable_points
-                return Response({"message":'已登入','success':True, 'points':points}, status=200)
+                return Response({"messages":'已登入','success':True, 'points':points}, status=200)
             else:
-                return Response({"message": '請登入', 'success': False}, status=200)
+                return Response({"messages": '請登入', 'success': False}, status=200)
         else:
-            return Response({"message": '未登入', 'success': False}, status=200)
+            return Response({"messages": '未登入', 'success': False}, status=200)
 
     def post(self,request):
         username = request.POST.get('username', '')
@@ -105,7 +105,7 @@ class LoginView(APIView):
         if user is not None and user.is_active:
             request.session['username'] = username
             if user.email == '':
-                return Response({"message": '信箱尚未認證', 'success': False}, status=200)
+                return Response({"messages": '信箱尚未認證', 'success': False}, status=200)
 
             auth.login(request, user)
             try:
@@ -113,9 +113,9 @@ class LoginView(APIView):
             except ObjectDoesNotExist:
                 ProfileUser = UserProfile.objects.create(user=user)
             points = ProfileUser.usable_points
-            return Response({"message":'登入成功','success':True,'user':user.username,'points':points}, status=200)
+            return Response({"messages":'登入成功','success':True,'user':user.username,'points':points}, status=200)
         else:
-            return Response({"message": '使用者名稱或密碼有誤', 'success': False}, status=200)
+            return Response({"messages": '使用者名稱或密碼有誤', 'success': False}, status=200)
 
 class LogoutView(View):
     def get(self, request):
