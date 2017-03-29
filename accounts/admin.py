0607b3356ed6d126857a047696a809dc1e50727b
Hanson <hnhn789@yahoo.com.tw>
2017-03-29 17:44:55 +0800

    good

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@ from accounts.models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
 
-    list_display = ["user", "usable_points","stories"]
+    list_display = ["user", "usable_points","history_points","stories"]
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
